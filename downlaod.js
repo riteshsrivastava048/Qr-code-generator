a80414d943f2fd151609cab4ac22b23fa39a685d
riteshsrivastava048@gmail.com <2023041247@mmmut.ac.in>
2024-09-25 23:01:58 +0530

    25/09

--- downlaod.js
+++ downlaod.js
@@ -3,6 +3,6 @@ btn.addEventListener("click",function(){
     const imgsrc = document.querySelector(".image").src ;
     const link = document.createElement('a')
     link.href = imgsrc ;
-    link.download = "imagegenerated.png" ;
+    link.download = "public/imagegenerated.png" ;
     link.click();
 })
